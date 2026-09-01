3068c750ad8ef05f9e64cbcfb2a81ff77caf55eb
MD Sami <mdsamikhan0143@gmail.com>
2026-09-01 16:58:38 +0530

    Fix frontend CORS URL

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://ai-resume-analyzer-frontend-n8a5.onrender.com",
-    ],
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
